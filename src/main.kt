09794643c39dfc88689b02d8150970721e428095
danadora86 <dorache_dana@yahoo.com>
2025-10-25 15:44:59 +0100

    add eat fun

--- src/main.kt
+++ src/main.kt
@@ -1,8 +1,17 @@
 fun main() {
     val binnie = Cat("Binnie", 10, 10)
     val clyde = Cat("Clyde", 5, 5)
+    val flathead = Cat("flathead", 7, 10)
+    val cupra = Cat("cupra", 2, 7)
+
     binnie.walk()
     clyde.walk()
+    flathead.walk()
+    cupra.walk()
+
     println(binnie)
     println(clyde)
+    println(flathead)
+    println(cupra)
+
 }
